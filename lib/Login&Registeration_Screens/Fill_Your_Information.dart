d9b8e19cefa927aa5eb0156c97d96da44f2befdb
project calculator <ritamagdy99@gmail.com>
2023-11-19 02:38:41 +0200

    Form Label

--- lib/Login&Registeration_Screens/Fill_Your_Information.dart
+++ lib/Login&Registeration_Screens/Fill_Your_Information.dart
@@ -7,14 +7,38 @@ class FillYourInformation extends StatelessWidget {
 
   static const RouteName = "FillYourInformation";
 
-  late PickedFile imageFile;
-  final ImagePicker picker = ImagePicker();
+  TextEditingController  namecontroller= TextEditingController();
+  TextEditingController  phonecontroller= TextEditingController();
+  TextEditingController  emailcontroller= TextEditingController();
+  TextEditingController  passwordcontroller= TextEditingController();
+  TextEditingController  repasswordcontroller= TextEditingController();
 
+  var formkey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+    body: Column(
+      children: [
+     Padding(
+       padding: const EdgeInsets.all(8.0),
+       child: Padding(
+         padding: const EdgeInsets.all(8.0),
+         child: SingleChildScrollView(
+         child: Form(
+             key:formkey,
+           child: Column(
+             crossAxisAlignment: CrossAxisAlignment.stretch,
+             children: [
+Form
+             ],
 
+           )),
 
+         ),
+       ),
+     )
+      ],
+    ),
     );
 
 
---
+++ lib/widget/Form_Label_Widget.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class FormLabelWidget extends StatelessWidget {
+
+  String Label;
+  FormLabelWidget({required this.Label});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      Label,
+      style: TextStyle(fontSize: 24,color: Colors.black87),
+
+    );
+  }
+}
