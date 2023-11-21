ebb049b6c60a635dc220182f603a040b48b35f07
project calculator <ritamagdy99@gmail.com>
2023-11-22 00:28:50 +0200

    Book Now is ready

--- lib/Book_Now/Book_Now.dart
+++ lib/Book_Now/Book_Now.dart
@@ -6,7 +6,31 @@ class BookNow extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-   return Scaffold();
+   return Scaffold(
+     body: Padding(
+       padding: EdgeInsets.all(15),
+       child: Column(
+         mainAxisAlignment: MainAxisAlignment.center,
+         crossAxisAlignment: CrossAxisAlignment.stretch,
+         children: [
+           Image.asset("assets/images/BookNow.png",
+             width: 300,
+             height: 300,
+           ),
 
+           SizedBox(height: 30,width: 30,),
+           ElevatedButton(
+             onPressed: () { },
+             style: ButtonStyle(
+               backgroundColor: MaterialStateProperty.all(Colors.black),
+               minimumSize: MaterialStateProperty.all(Size(340, 50)),
+             ),
+             child: Text('Book Now'),
+           ),
+
+         ],
+       ),
+     ),
+   );
   }
 }
